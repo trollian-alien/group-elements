2767513334ce632c09fe25b9cfc689711393510f
trollian-alien <epciscientician@gmail.com>
2025-11-17 15:18:13 +0300

    Reworked the word class and added some tests

--- group_class.py
+++ group_class.py
@@ -1,60 +1,73 @@
 from constants import PRODUCE_RELATORS
 
-class Word: #models words. inputs will actually be integers. see group class for explanations.
-    def __init__(self, iterable):
-        l = list(iterable)
-        if not all(isinstance(x, int) for x in l):
-            raise TypeError("Word letters must be integers")
-        t = [x for x in l if x != 0]
-        self.word = tuple(t)
-        self.length = len(t)
-        self.gens = max(abs(n) for n in t) #number of generators used
+def is_ordered_pair(x): #helper function to check that the Words have correct syntax
+    return (
+        isinstance(x, tuple) and
+        len(x) == 2 and
+        isinstance(x[0], int) and x[0] >= 0 and
+        isinstance(x[1], int)
+    )
 
-    def is_subword(self,other): #checks if self is subword of other
-        if other.length < self.length:
-            return False
-        for i in range(0, other.length - self.length + 1):
-            if self.word == other.word[i: i + self.length]:
-                return True #match found
-        return False #all subwords don't match
+class Word: #models words. inputs will actually be integers. see group class for explanations.
+    def __init__(self, iterable): #iterable consists of ordered pairs of integers (the first non-negative), the first is the generator, the second the exponent
+        try:
+            l = list(iterable)
+        except:
+            raise TypeError("Please list the elements of the word in a list or tuple")
+        if not all(is_ordered_pair(x) for x in l):
+            raise TypeError("Invalid word. Please write words as a list of (generator, exponent) integer pair, where the generator int is >=0")
+
+        t = [x for x in l if x[0] != 0 and x[1]!= 0]
+        self.word = tuple(t)
+        self.length = sum(abs(x[1]) for x in t)
+        self.gens = max(n[0] for n in t) #number of generators used
+
+    def __eq__(self, other):
+        return self.reduce().word == other.reduce().word
 
     def __repr__(self):
         return self.word.__repr__()
     
-    def reduce(self): #turns the word into a reduced one. Other operations reduce the word too
+    def reduce(self): #turns the word into a reduced one. used in other operations too.
         reduced_self = []
         i = 0
         for x in self.word:
-            if reduced_self and reduced_self[-1] == -x:
-                reduced_self.pop()
+            if reduced_self and reduced_self[-1][0] == x[0]:
+                reduced_self[-1] = (x[0], x[1] + reduced_self[-1][1]) #the __init__ method gets rid of (n, 0)s so we can keep them here
             else:
                 reduced_self.append(x)
         return Word(reduced_self)
+    
+    def is_subword(self,other): #checks if self is subword of other by first reducing them
+        self, other = self.reduce(), other.reduce()
+        if other.length < self.length:
+            return False
+        for i in range(0, len(other.word) - len(self.word) + 1):
+            if self.word == other.word[i: i + len(self.word)]:
+                return True #match found
+        return False #all subwords don't match
                 
-    def __mul__(self, other): #performs the free product of both elements, result is reduced if both self and other are reduced
-        length = min(len(self.word), len(other.word))
-        x, y = self.word, other.word
-        for i in range(0,length):
-            if self.word[-1-i] == -other.word[i]:
-                x, y = x[:-1], y[1:]
-            else:
-                return Word(x+y)
-        return Word(x+y)
+    def __mul__(self, other): #performs the free product of both elements, result is reduced if self and other are reduced
+        return Word(self.word+other.word).reduce()
     
             
-    def inv(self): #inverses
-        return Word([-self.word[self.length-1-i] for i in range(0,self.length)])
+    def inv(self): #inverses.
+        return Word([(self.word[len(self.word)-1-i][0], -self.word[len(self.word)-1-i][1]) for i in range(0,len(self.word))])
     
     def conj(self, other): #conjugates self by other
         return other.inv() * self * other
     
     def pow(self, n: int):
+        power = tuple()
         if n == 0:
             return Word([])
         elif n > 0:
-            return self * self.pow(n-1)
+            for i in range(0,n):
+                power += self.word
         elif n < 0:
-            return self.inv() * self.pow(n+1)
+             for i in range(0,-n):
+                power += self.inv().word
+        return Word(power).reduce()
 
 
 class Group: #This group class is defined from a presentation
@@ -104,7 +117,7 @@ class Group: #This group class is defined from a presentation
 
     # Tietze Operations; to be finished
     def add_generator(self):
-        self.generators.append(len(self.generators))
+        pass
 
     def add_generators(self, n):
         for i in range(0, n):
---
+++ tests/test_group_class.py
@@ -0,0 +1,43 @@
+from group_class import Word
+import unittest
+
+class TestWord(unittest.TestCase):
+
+    def test_init(self):
+        word = Word(((1,4),(2,6),(0,2),(1,0),(1,-5)))
+        self.assertEqual(word.word, ((1,4),(2,6),(1,-5)))
+        self.assertEqual(word, Word(((1,4),(2,6),(1,-5))))
+        self.assertEqual(word.length, 4+6+5)
+        self.assertEqual(word.gens, 2)
+        with self.assertRaises(TypeError):
+            Word(((-1,2),(3,1)))
+
+    def test_reduce(self):
+        word = Word(((1,2),(1,-2),(3,5),(3,6),(2,1),(2,-4),(2,2)))
+        self.assertEqual(word.reduce().word, ((3,11),(2,-1)))
+
+    def test_subword(self):
+        word = Word(((1,2),(2,-3),(6,-7),(3,1),(4,7)))
+        subword1 = Word(((6,-7),(3,1)))
+        subword2 = Word(((2,-3),))
+        self.assertTrue(subword1.is_subword(word))
+        self.assertTrue(subword2.is_subword(word))
+        self.assertFalse(subword2.is_subword(subword1))
+
+    def test_mul(self):
+        word1 = Word(((1,2),(2,-3)))
+        word2 = Word(((2, 3), (1,1)))
+        self.assertEqual(word1 * word2, Word(((1,3),)))
+
+    def test_inv(self):
+        word = Word(((1,2),(2,1),(1,-3)))
+        self.assertEqual(word.inv(), Word(((1,3),(2,-1),(1,-2))))
+
+    def test_pow(self):
+        word = Word(((1,2),(2,1),(1,-3)))
+        self.assertEqual(word.pow(3), Word(((1,2),(2,1),(1,-1),(2,1),(1,-1),(2,1),(1,-3))))
+        self.assertEqual(word.pow(-3), Word(((1,2),(2,1),(1,-1),(2,1),(1,-1),(2,1),(1,-3))).inv())
+
+
+if __name__ == '__main__':
+    unittest.main()
